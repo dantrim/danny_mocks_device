2f6aa75f3926c1375dc7466f51e9808b1ccbae00
dantrim <dantrim1023@gmail.com>
2021-01-22 17:18:54 -0800

    join

--- scripts/test_run.py
+++ scripts/test_run.py
@@ -9,7 +9,9 @@ def main() :
     port = device.open()
     print(f"Opened port: {port}")
     device.start()
-    input()
+    # need a proper thread halt here!! python does this weird
+    device.stop()
+    device.join()
 
 if __name__ == "__main__" :
     main()
